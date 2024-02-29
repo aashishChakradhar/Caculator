b796376c6a564c9291eafaa8002766b2b2f882af
aashishChakra <ashishchakradhar00@gmail.com>
2024-02-29 21:43:59 +0545

    this a simple calculator using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,219 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useState,
+} from "react"; 
+import "./App.css";
 
-function App() {
-  return (
+function App() { 
+  const[number, setNumber]=useState({
+    input: 0,
+    symbol: null,
+    result: null,
+  });
+
+  function clickHandel(){
+    if(number.symbol == null){
+      setNumber({
+        ...number,input: ' ',result: null
+      })
+    }
+    else{
+      setNumber({
+        ...number,input: ' ',
+      })
+    }
+  }
+
+  function Equals(){
+    if (number.result === 0){
+      setNumber({
+        result: null
+      })
+    }
+    if(number.input === ' '){
+      number.input = 0
+    }
+    if (number.symbol === '+'){
+      setNumber(
+        {
+          ...number,
+          result : parseFloat(number.result) + parseFloat(number.input),
+          input:0,
+          symbol: null
+        }
+      )      
+    }
+    else if (number.symbol === '-'){
+      setNumber(
+        {
+          ...number,
+          result : parseFloat(number.result) - parseFloat(number.input),
+          input:0,
+          symbol: null
+        }
+      )  
+    }
+    else if (number.symbol === '*'){
+      setNumber(
+        {
+          ...number,
+          result : parseFloat(number.result) * parseFloat(number.input),
+          input:0,
+          symbol: null
+        }
+      )  
+    }
+    else if (number.symbol === '/'){
+      setNumber(
+        {
+          ...number,
+          result : parseFloat(number.result) / parseFloat(number.input),
+          input:0,
+          symbol: null
+        }
+      )  
+    }
+    else{
+      number.result = number.input
+    }
+  };
+
+  function Add(){
+    if(number.input === ' '){
+      number.input = 0
+    }
+    if(number.result === null){
+      setNumber({
+        ...number,
+        result: parseFloat(number.input),
+        symbol: '+',
+      })
+    }
+    else{
+      setNumber({
+        ...number,
+        result: parseFloat(number.result) + parseFloat(number.input),
+        symbol: '+',
+      })
+    }
+  };
+
+  function Subtract(){
+    if(number.input === ' '){
+      number.input = 0
+    }
+    if(number.result === null){
+      setNumber({
+        ...number,
+        result : number.input,
+        symbol: '-',
+      })
+    }
+    else{
+      setNumber({
+        ...number,
+        result: parseFloat(number.result) - parseFloat(number.input) ,
+        symbol: '-',
+      })
+    }
+  };
+
+  function Times(){
+    if(number.input === ' '){
+      number.input = 0
+    }
+    if(number.result === null){
+      setNumber({
+        ...number,
+        result: parseFloat(number.input),
+        symbol: '*',
+      })
+    }
+    else{
+      setNumber({
+        ...number,
+        result: parseFloat(number.result) * parseFloat(number.input),
+        symbol: '*',
+      })
+    }
+  };
+
+  function Division(){
+    if(number.input === ' '){
+      number.input = 0
+    }
+    if(number.result === null){
+      setNumber({
+        ...number,
+        result: parseFloat(number.input),
+        symbol: '/',
+      })
+    }
+    else{ 
+      setNumber({
+        ...number,
+        result: parseFloat(number.result) / parseFloat(number.input),
+        symbol: '/',
+      })
+    }
+  };
+
+  function Power(power){
+    setNumber({
+      ...number,
+      result : Math.pow(number.input,power),  
+      symbol: null
+    })
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1>Simple Working Calculator</h1>
+      <p> = {number.result} {number.symbol} </p>
+      <input type = 'number' placeholder="0" pattern="[0-9]"
+        onClick={
+          clickHandel
+        }
+        value={number.input}
+        onChange={
+          e => {
+            setNumber({
+                ...number,
+                input: e.target.value,
+              }
+            )
+          } 
+        }   
+      />
 
-export default App;
+      <div>
+        <button onClick={()=>Power(2)}>X<sup>2</sup></button>
+        <button onClick={()=>Power(0.5)}>X<sup>1/2</sup></button>
+        <button onClick={Add}>+</button>
+        <button onClick={Subtract}>-</button>
+        <button onClick={Times}>*</button>
+        <button onClick={Division}>/</button>
+        <button onClick={Equals}>=</button>
+
+        <button 
+          onClick={
+            () => setNumber({
+              ...number,input: 0,
+            })
+          }
+        >
+          Clear
+        </button>
+
+        <button 
+          onClick={
+            () => setNumber({
+              ...number, result: null, symbol: null,input: 0
+            })
+          }
+          >
+          MC
+        </button>
+      </div>
+    </div>
+  )
+} 
+export default App; 
